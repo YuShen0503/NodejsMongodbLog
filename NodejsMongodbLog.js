b5e65dcbad80121527abbb1a4096e49e21ead6ec
YuShen <106447867@qq.com>
2021-12-08 17:30:10 -0800

    Nothing

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,14 +1,16 @@
 const express = require("express")
 const app = express()
-// const mongose = require('mongoose');
-// mongoose.connect('mongodb://localhost/test');
-// const schema={
-//     name:String,
-//     age:Number,
-//     hobby:String,
-//     health:String
-// }
-// const Cat = mongoose.model('cats', schema);
+const mongose = require('mongoose');
+mongoose.connect('mongodb://localhost/test1');
+const schema={
+    name:String,
+    age:Number,
+    hobby:String,
+    health:String
+}
+const Cat = mongoose.model('cats1', schema);
+const kitty = new Cat({ name: 'Zildjian2' });
+kitty.save().
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
