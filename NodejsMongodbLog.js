629772b0a7f09ac9d0e3913411071c6053cf0d91
YuShen <106447867@qq.com>
2021-12-08 17:17:22 -0800

    For pratice

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,5 +1,14 @@
 const express = require("express")
 const app = express()
+// const mongose = require('mongoose');
+// mongoose.connect('mongodb://localhost/test');
+// const schema={
+//     name:String,
+//     age:Number,
+//     hobby:String,
+//     health:String
+// }
+// const Cat = mongoose.model('cats', schema);
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
