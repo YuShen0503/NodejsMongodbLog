7c65fd59b11d2cd63f49ff6a24c1114a12de4e10
YuShen <106447867@qq.com>
2021-12-12 17:36:32 -0800

    ejs success

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const mongoose = require('mongoose');
+const ejs = require('ejs')
 mongoose.connect('mongodb://172.21.2.236:27017/190110910414');
 const schema={
     name:String,
@@ -8,15 +9,19 @@ const schema={
     hobby:String,
     health:String
 }
-const Cat = mongoose.model('cats1', schema);
+const mydata = mongoose.model('cats1', schema);
 // const kitty = new Cat({ name: 'Zildjian2' });
 // kitty.save()
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
-    res.send(req.query)
+    //res.send(req.query)
     console.log(req.query)
     const kitty = new mydata({ name: req.query.first,health:req.query.second });
     kitty.save()
-    
+    // ejs.renderFile(filename, data, options, function(err, str){
+    //     // str => 输出渲染后的 HTML 字符串
+    // });
+    ejs.renderFile("result.html",{returnVAl:"success"}<{req,res})
+        res.send(str)
 })
 app.listen(10414)
